c0615f302fdeafadcbd280e84f083a22c3a37292
Sina Tayebi <sinaapolo969@gmail.com>
2022-06-05 21:59:54 +0430

    setup clienthandler and the basic connection!

--- src/Network/Client.java
+++ src/Network/Client.java
@@ -1,6 +1,25 @@
 package Network;
 
+import java.io.*;
+import java.net.Socket;
+
 public class Client
 {
-
+    private Socket socket;
+    private DataOutputStream dataOutputStream;
+    private DataInputStream dataInputStream;
+    public void setUp()
+    {
+        try
+        {
+            socket = new Socket("127.0.0.1", 6666);
+            dataInputStream = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+            dataOutputStream = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
+            //we should call the launch method here and send the requests for server
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
 }
