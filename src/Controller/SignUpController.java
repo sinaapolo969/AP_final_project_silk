701e2f5d727cb149d62c0ba57f11b4bab9421d51
ariananaira <cogokeye@gmail.com>
2022-06-07 20:23:42 +0430

    file chooser feature

--- src/Controller/SignUpController.java
+++ src/Controller/SignUpController.java
@@ -1,24 +1,33 @@
 package Controller;
 
 import Model.Common;
+import Model.PageControl;
 import com.jfoenix.controls.JFXDrawer;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.VBox;
-import javafx.scene.paint.ImagePattern;
-import javafx.scene.shape.Circle;
+import javafx.stage.FileChooser;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class SignUpController implements Initializable
 {
+    @FXML
+    private ImageView chosen;
+
     @FXML
     private JFXTextField lastName;
 
@@ -39,9 +48,6 @@ public class SignUpController implements Initializable
 
     @FXML
     private JFXTextField username;
-
-    @FXML
-    private Circle common;
 
     @FXML
     private JFXDrawer leftDrawer;
@@ -76,12 +82,46 @@ public class SignUpController implements Initializable
         Common.openOrCloseDrawer(leftDrawer);
     }
 
+
+    @FXML
+    void fileChoose(ActionEvent event)
+    {
+        FileChooser fileChooser = new FileChooser();
+
+        //Set extension filter
+        FileChooser.ExtensionFilter extFilterJPG = new FileChooser.ExtensionFilter("JPG files (*.jpg)", "*.JPG");
+        FileChooser.ExtensionFilter extFilterPNG = new FileChooser.ExtensionFilter("PNG files (*.png)", "*.PNG");
+        fileChooser.getExtensionFilters().addAll(extFilterJPG, extFilterPNG);
+
+        //Show open file dialog
+        File file = fileChooser.showOpenDialog(null);
+
+        try
+        {
+            BufferedImage bufferedImage = ImageIO.read(file);
+            Image image = SwingFXUtils.toFXImage(bufferedImage, null);
+            chosen.setImage(image);
+        }
+        catch (IOException ex)
+        {
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE, null, ex);
+        }
+
+
+    }
+
+    @FXML
+    private void logIn(ActionEvent actionEvent) throws IOException
+    {
+        PageControl.open("Login");
+    }
+
     @Override
     public void initialize(URL location, ResourceBundle resources)
     {
         Common.initialDrawer("HomeMenu", leftDrawer);
 
-        Image profile = new Image(getClass().getResource("/View/user.png").toExternalForm());
-        common.setFill(new ImagePattern(profile));
+//        Image profile = new Image(getClass().getResource("/View/user.png").toExternalForm());
+//        common.setFill(new ImagePattern(profile));
     }
 }
