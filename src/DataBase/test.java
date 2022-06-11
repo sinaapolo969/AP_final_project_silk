45026eb7ed4ec51ac7604642ddfa5341b66db149
Sina Tayebi <sinaapolo969@gmail.com>
2022-06-11 23:56:50 +0430

    test

--- src/DataBase/test.java
+++ src/DataBase/test.java
@@ -30,25 +30,36 @@ public class test
             "}";
     static String json3 = "{\n" +
             "  \"title\": \"bib\",\n" +
+            "  \"postId\": \"2\", \n " +
             "  \"category\": \"home\",\n" +
             "  \"description\": \"something\",\n" +
             "  \"price\": \"10000\",\n" +
             "  \"owner\": \"sina\",\n" +
+            "  \"location\": \"ahvaz\", \n" +
             "  \"sold\": \"0\",\n" +
             "  \"photo\": \"something\"\n" +
             "}";
     public static void main(String[] args) throws SQLException, IOException, ClassNotFoundException
     {
-        ArrayList<Post> posts = new DataRequestSender().postRequestByCategory("home");
-//        UserTable userTable = new UserTable();
+        //ArrayList<Post> posts = new DataRequestSender().postRequestByCategory("home");
+//        PostTable postTable = new PostTable();
+//        ArrayList<String> posts = postTable.getPostDataByOwner("sina");
+//        JSONObject jsonObject = new JSONObject(posts.get(0));
+//        System.out.println(jsonObject.getString("title"));
 //        String json = userTable.getUserData("sina");
 //        JSONObject jsonObject = new JSONObject(json);
 //        System.out.println(jsonObject.getString("firstName"));
-        for (Post post : posts)
-        {
-            System.out.println(post.getCategory() + "   " + post.getTitle());
-        }
-//        PostTable postTable = new PostTable();
-//        System.out.println(postTable.getPostByCategory("home"));
+//        for (Post post : posts)
+//        {
+//            System.out.println(post.getCategory() + "   " + post.getTitle() +
+//                     post.getPostId() + "  " + post.getPhoneNumber() + "   " + post.getLocation());
+//        }
+        PostTable postTable = new PostTable();
+        ArrayList<String> posts = postTable.getPostByCategory("eih");
+        System.out.println(posts);
+        System.out.println("-------------------------------------");
+        System.out.println(postTable.getBookMarks("sinat"));
+        postTable.close();
+
     }
 }
