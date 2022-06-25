9778fb6728f320d7367115ddd729b96717acb9d1
ariananaira <cogokeye@gmail.com>
2022-06-25 23:11:39 +0430

    desining post pages

--- src/Controller/PostVisitController.java
+++ src/Controller/PostVisitController.java
@@ -1,5 +1,92 @@
 package Controller;
 
-public class PostVisitController
+import Model.Post;
+import com.jfoenix.controls.JFXDrawer;
+import com.jfoenix.controls.JFXTextArea;
+import com.jfoenix.controls.JFXTextField;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.shape.Circle;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class PostVisitController implements Initializable
 {
+    Post currentPost = new Post();
+
+    @FXML
+    private Circle common;
+
+    @FXML
+    private JFXTextField search;
+
+    @FXML
+    private JFXDrawer leftDrawer;
+
+    @FXML
+    private ImageView image;
+
+    @FXML
+    private Label title;
+
+    @FXML
+    private Label owner;
+
+    @FXML
+    private Label number;
+
+    @FXML
+    private JFXTextArea description;
+
+    @FXML
+    private JFXTextField category;
+
+    @FXML
+    private JFXTextField price;
+
+    @FXML
+    void copy(MouseEvent event)
+    {
+
+    }
+
+    @FXML
+    void instagram(MouseEvent event)
+    {
+
+    }
+
+    @FXML
+    void linkedIn(MouseEvent event)
+    {
+
+    }
+
+    @FXML
+    void menu(MouseEvent event)
+    {
+
+    }
+
+    @FXML
+    void twitter(MouseEvent event)
+    {
+
+    }
+
+    @FXML
+    void userMenu(MouseEvent event)
+    {
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources)
+    {
+
+    }
 }
