14f58c0482a51dd3c9390002ee0a685b9d155f8c
ariananaira <cogokeye@gmail.com>
2022-06-27 18:19:42 +0430

    enum comboBox test

--- src/Model/Post/Categories.java
+++ src/Model/Post/Categories.java
@@ -2,23 +2,12 @@ package Model.Post;
 
 public enum Categories
 {
-    RealEstate("Apartment", "Domain", "Studio Flat"),
-    Vehicle("Truck", "Car", "Cycles"),
-    Digital("Computer", "Mobile & Tablet", "Video & Audio Equipments"),
-    Furniture("Electronic", "Decorative", "Others"),
-    Service("Beauty", "Services", "Financial & Educational"),
-    Entertainment("Sports", "Musical", "Others"),
-    Recruitment("Education", "ُServices", "industrial"),
-    Personal("Fashion", "Beauty", "Stationery");
-
-    final String firstSubCategory;
-    final String secondSubCategory;
-    final String thirdSubCategory;
-
-    Categories(String firstSubCategory, String secondSubCategory, String thirdSubCategory)
-    {
-        this.firstSubCategory = firstSubCategory;
-        this.secondSubCategory =secondSubCategory;
-        this.thirdSubCategory = thirdSubCategory;
-    }
+    RealEstate,
+    Vehicle,
+    Digital,
+    Furniture,
+    Service,
+    Entertainment,
+    Recruitment,
+    Personal;
 }
